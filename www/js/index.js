d211e8a523fbd786e6d70b692908805d4324a91d
wap4444 <wap4444@mail.ru>
2017-08-14 13:40:14 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,49 +19,26 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
-        rr=1;
 function didReceiveRemoteNotificationCallBack(jsonData) {   
-rr=2;
-var ref = cordova.InAppBrowser.open(jsonData.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 }
-
 function didOpenRemoteNotificationCallBack(jsonData) {
-rr=2;
-//ref.close();
-// Для Andori
-//var newdata = JSON.parse ( jsonData.notification.payload.additionalData );
-//alert(newdata.ssylka);
-//var ref = cordova.InAppBrowser.open(newdata.ssylka , '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-//  Для Iphone
- var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 }
- 
-        
-        
         //Настройка ПУШЕЙ ДЛЯ АЙФОНА
         var iosSettings = {};
         iosSettings["kOSSettingsKeyAutoPrompt"] = true;
         iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
-
         //ПОДКЛЮЧЕНИЕ ПУШЕЙ
            window.plugins.OneSignal
-          .startInit("78520dbf-0cbd-41b3-aed3-1f27aba825e9")
+          .startInit("e1beb418-934b-4e48-b6df-4664b8b5a967")
           .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
           .handleNotificationOpened(didOpenRemoteNotificationCallBack)
               .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
           .iOSSettings(iosSettings)
           .endInit();
-        
         window.plugins.OneSignal.getIds(function(ids) {
-                    ipush = ids.userId;
-            if(rr==1)
-            {
-            var ref = cordova.InAppBrowser.open('http://aksu.controlsoft.kz/project_ksk/index.html?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-            }       
+            ipush = ids.userId;    
+            alert(ipush);
             });
-        
-
     }
 };
 
